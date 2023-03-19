f471af044fdfcac76d4be1b5086791c42a91c667
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-18 20:00:19 -0400

    followed guided project to add support for jwt to the api

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,11 +1,30 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../../config/index');
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({ status: 401, messsage: `token bad: ${err.message}` });
+      } else {
+        req.decodedJwt = decoded;
+        next();
+      }
+    })
+  } else {
+    next({ status: 401, message: 'what? no token?' });
+  }
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+const checkRole = role => (req, res, next) => {
+  if (req.decodedJwt && req.decodedJwt.role === role) {
+    next();
+  } else {
+    next({ status: 403, message: 'you have no power here' });
+  }
 }
 
 module.exports = {
